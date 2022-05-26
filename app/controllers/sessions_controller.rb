4bc7ed2eb4d45716cdcff7df25d7bd6909f51608
vickyruud <aprajit.k@gmail.com>
2022-05-26 12:35:45 -0400

    fix bug with login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,12 @@ class SessionsController < ApplicationController
 def login
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
+      puts 'here'
         payload = {user_id: user.id}
         token = encode_token(payload)
         render json: {user: user, jwt: token, success: "Welcome back, #{user.username}"}
     else
-        render json: {failure: "Log in failed! Username or password invalid!"}
+      render json: {errors: 'Invalid username or password!'}, status: :not_acceptable
     end
   end
 
